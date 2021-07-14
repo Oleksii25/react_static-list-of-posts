7539361f32fe841f1d5ac30805b4816707aefcc8
Oleksii Lymonov <alex25_92@ukr.net>
2021-07-14 17:36:27 +0300

    Updates

---
+++ precache-manifest.6c5a8a8e6c8ca9ddd21db4a1541285f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7ac9fd50f45565fec523fb8e70ba00",
+    "url": "./index.html"
+  },
+  {
+    "revision": "052c74803275c29d04f9",
+    "url": "./static/css/main.7f31a14a.chunk.css"
+  },
+  {
+    "revision": "5dfa23033ebc7f598c45",
+    "url": "./static/js/2.de80f401.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.de80f401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052c74803275c29d04f9",
+    "url": "./static/js/main.77a585fe.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
